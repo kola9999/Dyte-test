52d2dd30e23160709a279d573be839d76faa95a0
kola9999 <39917725+kola9999@users.noreply.github.com>
2021-07-17 21:29:06 +0530

    removed original credentials

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import "../src/components/index.css";
 
 //firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyBdVy7Tn4HniT6NMzvARzp6qRbF3mHmhRM",
-  authDomain: "potent-symbol-233716.firebaseapp.com",
-  databaseURL: "https://potent-symbol-233716.firebaseio.com",
-  projectId: "potent-symbol-233716",
-  storageBucket: "potent-symbol-233716.appspot.com",
-  messagingSenderId: "1075928338495",
-  appId: "1:1075928338495:web:b0ca7383d74ae5e030a88a"
+ apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID 
 };
 firebase.initializeApp(firebaseConfig);
 
